9465362310e5434780585067dca15d22a44b6533
labonirahman <rahmanlaboni243@gmail.com>
2026-05-24 21:19:50 +0600

    Update generated compiler outputs and runtime artifacts

---
+++ output.py
@@ -0,0 +1,27 @@
+# ============================================================
+# Generated Python target code
+# Produced by Bhasha Compiler Review 2
+# Compiler implementation language: Java
+# Target language: Python
+# ============================================================
+
+v0 = 10
+v1 = 5
+v2 = (v0 + v1)
+v3 = (v0 - v1)
+v4 = (v0 * v1)
+v5 = ((v0 + v1) * 2)
+v0 = (v0 + 2)
+v6 = "হিসাব সম্পন্ন"
+print("যোগফল:")
+print(v2)
+print("বিয়োগফল:")
+print(v3)
+print("গুণফল:")
+print(v4)
+print("জটিল ফল:")
+print(v5)
+print("আপডেটেড প্রথম:")
+print(v0)
+print("চূড়ান্ত বার্তা:")
+print(v6)
