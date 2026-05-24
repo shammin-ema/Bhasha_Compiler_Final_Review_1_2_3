7c72746dc981ac48a22a7bff2a5510f104c9d043
labonirahman <rahmanlaboni243@gmail.com>
2026-05-24 18:17:56 +0600

    Refactor parser structure and improve helper methods laboni updated

--- Parser.java
+++ Parser.java
@@ -1,18 +1,37 @@
 import java.util.*;
+
 public class Parser {
+
+    // =========================
+    // Parser State
+    // =========================
 
     private final List<Token> tokens;
     private int pos = 0;
+
     private final SymbolTable symbolTable = new SymbolTable();
+
     public boolean hasError = false;
+
+    // =========================
+    // Constructor
+    // =========================
 
     public Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
 
+    // =========================
+    // Public Accessors
+    // =========================
+
     public SymbolTable getSymbolTable() {
         return symbolTable;
     }
+
+    // =========================
+    // Token Navigation Helpers
+    // =========================
 
     private Token peek() {
         return tokens.get(pos);
@@ -20,7 +39,10 @@ public class Parser {
 
     private Token peekAt(int offset) {
         int index = pos + offset;
-        return index < tokens.size() ? tokens.get(index) : tokens.get(tokens.size() - 1);
+
+        return index < tokens.size()
+                ? tokens.get(index)
+                : tokens.get(tokens.size() - 1);
     }
 
     private Token consume() {
@@ -37,6 +59,7 @@ public class Parser {
                 return true;
             }
         }
+
         return false;
     }
 
@@ -46,26 +69,55 @@ public class Parser {
         }
     }
 
+    private boolean isAddOperator() {
+        return check(TokenType.PLUS)
+                || check(TokenType.MINUS);
+    }
+
+    private boolean isMulOperator() {
+        return check(TokenType.MULTIPLY)
+                || check(TokenType.DIVIDE);
+    }
+
+    private boolean isStatementTerminator() {
+        return check(TokenType.NEWLINE)
+                || check(TokenType.SEMICOLON);
+    }
+
+    // =========================
+    // Program Parsing
+    // =========================
+
     public ASTNode parseProgram() {
-        ASTNode program = new ASTNode(ASTNode.NodeType.PROGRAM);
+
+        ASTNode programNode =
+                new ASTNode(ASTNode.NodeType.PROGRAM);
 
         skipNewlines();
 
         while (!check(TokenType.EOF)) {
-            ASTNode statement = parseStatement();
 
-            if (statement != null) {
-                program.statements.add(statement);
+            ASTNode statementNode = parseStatement();
+
+            if (statementNode != null) {
+                programNode.statements.add(statementNode);
             }
 
             skipNewlines();
         }
 
-        return program;
+        return programNode;
     }
 
+    // =========================
+    // Statement Parsing
+    // =========================
+
     private ASTNode parseStatement() {
-        if (check(TokenType.SHONKHA) || check(TokenType.BAKKHO)) {
+
+        if (check(TokenType.SHONKHA)
+                || check(TokenType.BAKKHO)) {
+
             return parseDeclaration();
         }
 
@@ -73,194 +125,367 @@ public class Parser {
             return parsePrint();
         }
 
-        if (check(TokenType.IDENTIFIER) && peekAt(1).type == TokenType.ASSIGN) {
+        if (check(TokenType.IDENTIFIER)
+                && peekAt(1).type == TokenType.ASSIGN) {
+
             return parseAssignment();
         }
 
-        if (check(TokenType.SEMICOLON) || check(TokenType.NEWLINE)) {
+        if (check(TokenType.SEMICOLON)
+                || check(TokenType.NEWLINE)) {
+
             consume();
             return null;
         }
 
         syntaxError("Unexpected token: " + peek().value);
+
         recoverToNewlineOrSemicolon();
+
         return null;
     }
 
+    // =========================
+    // Declaration Parsing
+    // =========================
+
     private ASTNode parseDeclaration() {
+
         Token typeToken = consume();
+
         String dataType = typeToken.value;
 
         Token nameToken = expect(TokenType.IDENTIFIER);
+
         expect(TokenType.ASSIGN);
 
-        ASTNode expression = parseExpression();
+        ASTNode expressionNode = parseExpression();
 
-        String expressionType = inferType(expression);
+        String expressionType =
+                inferType(expressionNode);
 
-        if (expressionType != null && !dataType.equals(expressionType)) {
-            semanticError("Type mismatch: variable '" + nameToken.value
-                    + "' is " + dataType + " but value is " + expressionType, nameToken.line);
+        if (expressionType != null
+                && !dataType.equals(expressionType)) {
+
+            semanticError(
+                    "Type mismatch: variable '"
+                            + nameToken.value
+                            + "' is "
+                            + dataType
+                            + " but value is "
+                            + expressionType,
+
+                    nameToken.line
+            );
         }
 
-        symbolTable.declare(nameToken.value, dataType, nameToken.line);
+        symbolTable.declare(
+                nameToken.value,
+                dataType,
+                nameToken.line
+        );
 
-        ASTNode node = new ASTNode(ASTNode.NodeType.DECL, nameToken.value);
-        node.dataType = dataType;
-        node.line = nameToken.line;
-        node.right = expression;
+        ASTNode declarationNode =
+                new ASTNode(
+                        ASTNode.NodeType.DECL,
+                        nameToken.value
+                );
 
-        return node;
+        declarationNode.dataType = dataType;
+        declarationNode.line = nameToken.line;
+        declarationNode.right = expressionNode;
+
+        return declarationNode;
     }
 
+    // =========================
+    // Assignment Parsing
+    // =========================
+
     private ASTNode parseAssignment() {
+
         Token nameToken = consume();
 
         if (!symbolTable.isDeclared(nameToken.value)) {
-            semanticError("Undeclared variable: " + nameToken.value, nameToken.line);
+
+            semanticError(
+                    "Undeclared variable: "
+                            + nameToken.value,
+
+                    nameToken.line
+            );
         }
 
         expect(TokenType.ASSIGN);
 
-        ASTNode expression = parseExpression();
+        ASTNode expressionNode = parseExpression();
 
-        String variableType = symbolTable.typeOf(nameToken.value);
-        String expressionType = inferType(expression);
+        String variableType =
+                symbolTable.typeOf(nameToken.value);
 
-        if (variableType != null && expressionType != null && !variableType.equals(expressionType)) {
-            semanticError("Type mismatch in assignment: " + nameToken.value, nameToken.line);
+        String expressionType =
+                inferType(expressionNode);
+
+        if (variableType != null
+                && expressionType != null
+                && !variableType.equals(expressionType)) {
+
+            semanticError(
+                    "Type mismatch in assignment: "
+                            + nameToken.value,
+
+                    nameToken.line
+            );
         }
 
-        ASTNode node = new ASTNode(ASTNode.NodeType.ASSIGN, nameToken.value);
-        node.line = nameToken.line;
-        node.right = expression;
+        ASTNode assignmentNode =
+                new ASTNode(
+                        ASTNode.NodeType.ASSIGN,
+                        nameToken.value
+                );
 
-        return node;
+        assignmentNode.line = nameToken.line;
+        assignmentNode.right = expressionNode;
+
+        return assignmentNode;
     }
+
+    // =========================
+    // Print Statement Parsing
+    // =========================
 
     private ASTNode parsePrint() {
+
         Token printToken = consume();
 
-        ASTNode node = new ASTNode(ASTNode.NodeType.PRINT);
-        node.line = printToken.line;
-        node.right = parseExpression();
+        ASTNode printNode =
+                new ASTNode(ASTNode.NodeType.PRINT);
 
-        return node;
+        printNode.line = printToken.line;
+
+        printNode.right = parseExpression();
+
+        return printNode;
     }
 
+    // =========================
+    // Expression Parsing
+    // =========================
+
     private ASTNode parseExpression() {
-        ASTNode left = parseTerm();
 
-        while (check(TokenType.PLUS) || check(TokenType.MINUS)) {
-            Token opToken = consume();
-            String operator = opToken.value;
+        ASTNode leftNode = parseTerm();
 
-            ASTNode right = parseTerm();
+        while (isAddOperator()) {
 
-            ASTNode node = new ASTNode(ASTNode.NodeType.BINOP, operator);
-            node.line = opToken.line;
-            node.left = left;
-            node.right = right;
+            Token operatorToken = consume();
 
-            left = node;
+            String operator = operatorToken.value;
+
+            ASTNode rightNode = parseTerm();
+
+            leftNode = createBinaryNode(
+                    operator,
+                    leftNode,
+                    rightNode,
+                    operatorToken.line
+            );
         }
 
-        return left;
+        return leftNode;
     }
 
     private ASTNode parseTerm() {
-        ASTNode left = parsePrimary();
 
-        while (check(TokenType.MULTIPLY) || check(TokenType.DIVIDE)) {
-            Token opToken = consume();
-            String operator = opToken.value;
+        ASTNode leftNode = parsePrimary();
 
-            ASTNode right = parsePrimary();
+        while (isMulOperator()) {
 
-            ASTNode node = new ASTNode(ASTNode.NodeType.BINOP, operator);
-            node.line = opToken.line;
-            node.left = left;
-            node.right = right;
+            Token operatorToken = consume();
 
-            left = node;
+            String operator = operatorToken.value;
+
+            ASTNode rightNode = parsePrimary();
+
+            leftNode = createBinaryNode(
+                    operator,
+                    leftNode,
+                    rightNode,
+                    operatorToken.line
+            );
         }
 
-        return left;
+        return leftNode;
     }
 
+    // =========================
+    // Primary Expression Parsing
+    // =========================
+
     private ASTNode parsePrimary() {
-        Token token = peek();
+
+        Token currentToken = peek();
 
         if (check(TokenType.NUMBER)) {
+
             consume();
 
-            ASTNode node = new ASTNode(ASTNode.NodeType.NUMBER, token.value);
-            node.line = token.line;
-            return node;
+            ASTNode numberNode =
+                    new ASTNode(
+                            ASTNode.NodeType.NUMBER,
+                            currentToken.value
+                    );
+
+            numberNode.line = currentToken.line;
+
+            return numberNode;
         }
 
         if (check(TokenType.STRING)) {
+
             consume();
 
-            ASTNode node = new ASTNode(ASTNode.NodeType.STRING, token.value);
-            node.line = token.line;
-            return node;
+            ASTNode stringNode =
+                    new ASTNode(
+                            ASTNode.NodeType.STRING,
+                            currentToken.value
+                    );
+
+            stringNode.line = currentToken.line;
+
+            return stringNode;
         }
 
         if (check(TokenType.IDENTIFIER)) {
+
             consume();
 
-            if (!symbolTable.isDeclared(token.value)) {
-                semanticError("Undeclared variable: " + token.value, token.line);
+            if (!symbolTable.isDeclared(currentToken.value)) {
+
+                semanticError(
+                        "Undeclared variable: "
+                                + currentToken.value,
+
+                        currentToken.line
+                );
             }
 
-            ASTNode node = new ASTNode(ASTNode.NodeType.IDENTIFIER, token.value);
-            node.line = token.line;
-            return node;
+            ASTNode identifierNode =
+                    new ASTNode(
+                            ASTNode.NodeType.IDENTIFIER,
+                            currentToken.value
+                    );
+
+            identifierNode.line = currentToken.line;
+
+            return identifierNode;
         }
 
         if (check(TokenType.LPAREN)) {
+
             consume();
-            ASTNode expression = parseExpression();
+
+            ASTNode expressionNode = parseExpression();
+
             expect(TokenType.RPAREN);
-            return expression;
+
+            return expressionNode;
         }
 
-        syntaxError("Expected value but found: " + token.value);
+        syntaxError(
+                "Expected value but found: "
+                        + currentToken.value
+        );
+
         recoverToNewlineOrSemicolon();
 
-        ASTNode dummy = new ASTNode(ASTNode.NodeType.NUMBER, "0");
-        dummy.line = token.line;
-        return dummy;
+        ASTNode dummyNode =
+                new ASTNode(
+                        ASTNode.NodeType.NUMBER,
+                        "0"
+                );
+
+        dummyNode.line = currentToken.line;
+
+        return dummyNode;
     }
 
+    // =========================
+    // AST Helper Methods
+    // =========================
+
+    private ASTNode createBinaryNode(
+            String operator,
+            ASTNode leftNode,
+            ASTNode rightNode,
+            int line
+    ) {
+
+        ASTNode binaryNode =
+                new ASTNode(
+                        ASTNode.NodeType.BINOP,
+                        operator
+                );
+
+        binaryNode.line = line;
+        binaryNode.left = leftNode;
+        binaryNode.right = rightNode;
+
+        return binaryNode;
+    }
+
+    // =========================
+    // Type Inference
+    // =========================
+
     private String inferType(ASTNode node) {
+
         if (node == null) {
             return null;
         }
 
         return switch (node.nodeType) {
+
             case NUMBER -> "সংখ্যা";
 
             case STRING -> "বাক্য";
 
-            case IDENTIFIER -> symbolTable.typeOf(node.value);
+            case IDENTIFIER ->
+                    symbolTable.typeOf(node.value);
 
             case BINOP -> {
-                String leftType = inferType(node.left);
-                String rightType = inferType(node.right);
 
-                if (!"সংখ্যা".equals(leftType) || !"সংখ্যা".equals(rightType)) {
-                    semanticError("Arithmetic operator '" + node.value
-                            + "' requires সংখ্যা operands", node.line);
+                String leftType =
+                        inferType(node.left);
+
+                String rightType =
+                        inferType(node.right);
+
+                if (!"সংখ্যা".equals(leftType)
+                        || !"সংখ্যা".equals(rightType)) {
+
+                    semanticError(
+                            "Arithmetic operator '"
+                                    + node.value
+                                    + "' requires সংখ্যা operands",
+
+                            node.line
+                    );
+
                     yield null;
                 }
 
                 if ("/".equals(node.value)
                         && node.right != null
-                        && node.right.nodeType == ASTNode.NodeType.NUMBER
+                        && node.right.nodeType
+                        == ASTNode.NodeType.NUMBER
                         && "0".equals(node.right.value)) {
-                    semanticError("Division by zero", node.line);
+
+                    semanticError(
+                            "Division by zero",
+                            node.line
+                    );
+
                     yield null;
                 }
 
@@ -271,33 +496,88 @@ public class Parser {
         };
     }
 
+    // =========================
+    // Token Validation
+    // =========================
+
     private Token expect(TokenType expected) {
+
         if (check(expected)) {
             return consume();
         }
 
-        syntaxError("Expected " + expected + " but found " + peek().type);
+        syntaxError(
+                "Expected "
+                        + expected
+                        + " but found "
+                        + peek().type
+        );
+
         recoverToNewlineOrSemicolon();
-        return new Token(expected, "", peek().line);
+
+        return new Token(
+                expected,
+                "",
+                peek().line
+        );
     }
 
+    // =========================
+    // Error Recovery
+    // =========================
+
     private void recoverToNewlineOrSemicolon() {
-        while (!checkAny(TokenType.NEWLINE, TokenType.SEMICOLON, TokenType.EOF)) {
+
+        while (!checkAny(
+                TokenType.NEWLINE,
+                TokenType.SEMICOLON,
+                TokenType.EOF)) {
+
             consume();
         }
 
-        if (check(TokenType.NEWLINE) || check(TokenType.SEMICOLON)) {
+        if (isStatementTerminator()) {
             consume();
         }
+    }
+
+    // =========================
+    // Error Reporting
+    // =========================
+
+    private void reportError(
+            String errorType,
+            String message,
+            int line
+    ) {
+
+        System.err.println(
+                "[" + errorType + "] line "
+                        + line
+                        + ": "
+                        + message
+        );
+
+        hasError = true;
     }
 
     private void syntaxError(String message) {
-        System.err.println("[Syntax Error] line " + peek().line + ": " + message);
-        hasError = true;
+        reportError(
+                "Syntax Error",
+                message,
+                peek().line
+        );
     }
 
-    private void semanticError(String message, int line) {
-        System.err.println("[Semantic Error] line " + line + ": " + message);
-        hasError = true;
+    private void semanticError(
+            String message,
+            int line
+    ) {
+
+        reportError(
+                "Semantic Error",
+                message,
+                line
+        );
     }
 }
